44ebf4a2e11766dcac88a911309e37c119f4ac67
Patrick Goulding <patrickgoulding@gmail.com>
2019-08-19 15:53:20 -0600

    add route annotations
    co-authored-by: David Engel <davidjengel2012@gmail.com>

--- server.js
+++ server.js
@@ -17,28 +17,12 @@ app.get('/', (req, res) => {
   const { title } = app.locals
   res.status(200).send(`${title} is running on port ${app.get('port')}`)
 })
-
+// GETS
 app.get('/api/v1/projects', (req, res) => {
 
 })
 
 app.get('/api/v1/palettes', (req, res) => {
-
-})
-
-app.post('/api/v1/projects', (req, res) => {
-
-})
-
-app.post('/api/v1/palettes', (req, res) => {
-
-})
-
-app.put('/api/v1/projects/:id', (req, res) => {
-
-})
-
-app.put('/api/v1/palettes/:id', (req, res) => {
 
 })
 
@@ -50,6 +34,28 @@ app.get('/api/v1/palettes/:id', (req, res) => {
 
 })
 
+//POSTS
+
+app.post('/api/v1/projects', (req, res) => {
+
+})
+
+app.post('/api/v1/palettes', (req, res) => {
+
+})
+
+//PUTS
+
+app.put('/api/v1/projects/:id', (req, res) => {
+
+})
+
+app.put('/api/v1/palettes/:id', (req, res) => {
+
+})
+
+//DELETES
+
 app.delete('/api/v1/projects/:id', (req, res) => {
 
 })
@@ -57,3 +63,5 @@ app.delete('/api/v1/projects/:id', (req, res) => {
 app.delete('/api/v1/palettes/:id', (req, res) => {
 
 })
+
+//CUSTOM QUERY PARAM
