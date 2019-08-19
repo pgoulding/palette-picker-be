9ac34cde8687d1109689ba48fe9575407098e05c
Patrick Goulding <patrickgoulding@gmail.com>
2019-08-19 15:49:36 -0600

    add initial routes
    co-authored-by: David Engel <davidjengel2012@gmail.com>

--- server.js
+++ server.js
@@ -13,7 +13,47 @@ app.listen(app.get('port'), () => {
 
 app.locals.title = 'pallete picker'
 
-app.get('/api/v1/projects', (req, res) => {
+app.get('/', (req, res) => {
   const { title } = app.locals
-  res.status(200).json({title})
+  res.status(200).send(`${title} is running on port ${app.get('port')}`)
+})
+
+app.get('/api/v1/projects', (req, res) => {
+
+})
+
+app.get('/api/v1/palettes', (req, res) => {
+
+})
+
+app.post('/api/v1/projects', (req, res) => {
+
+})
+
+app.post('/api/v1/palettes', (req, res) => {
+
+})
+
+app.put('/api/v1/projects/:id', (req, res) => {
+
+})
+
+app.put('/api/v1/palettes/:id', (req, res) => {
+
+})
+
+app.get('/api/v1/projects/:id', (req, res) => {
+
+})
+
+app.get('/api/v1/palettes/:id', (req, res) => {
+
+})
+
+app.delete('/api/v1/projects/:id', (req, res) => {
+
+})
+
+app.delete('/api/v1/palettes/:id', (req, res) => {
+
 })
