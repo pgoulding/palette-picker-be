a149c27c9dc3b199bf67831081f4da045721e2c3
Patrick Goulding <patrickgoulding@gmail.com>
2019-08-21 15:26:26 -0600

    add body parser

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]
 const dbConnect = require('knex')(configuration)
 require('dotenv').config()
 app.use(express.json());
 app.use(cors())
-
+app.use(bodyParser.json())
 app.set('port', process.env.PORT || 3000)
 
 app.listen(app.get('port'), () => {
